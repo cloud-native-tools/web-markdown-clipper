9fb62dffdc1f2e21601b85cf81b8e589751313c2
liuqiming <john.liuqiming@gmail.com>
2023-01-17 02:05:43 +0800

    use json format for clips

--- src/utils.ts
+++ src/utils.ts
@@ -7,20 +7,16 @@ export const download_selection_as_markdown = () => {
     const markdown = turndown_service.turndown(html);
 
     const now = new Date();
-    const created = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-    const content = `---
-created: "${created}"
-source: "${document.baseURI}"
-title: "${document.title}"
----
+    const date = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    const clip = {
+        created: date,
+        source: document.baseURI,
+        title: document.title,
+        markdown: `[${document.title}](${document.baseURI})\n\n${markdown}`
+    };
 
-[${document.title}](${document.baseURI})
-
-${markdown}
-`;
-    const date = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
     const filename = `${document.title}-${date}`;
-    download_content(filename, content);
+    download_content(filename, JSON.stringify(clip));
 
     return markdown;
 };
@@ -61,60 +57,6 @@ export const download_content = (filename: string, content: string): void => {
     console.log(`download: [${link.download}]`);
     link.click();
 };
-
-export const copy_to_clipboard = async (content: string): Promise<void> => {
-    return await navigator.clipboard.writeText(content);
-};
-
-
-// export const download_image = (filename: string, url: string): void => {
-
-
-
-/* Link with a download attribute? CORS says no.
-var link = document.createElement('a');
-link.download = filename.substring(0, filename.lastIndexOf('.'));
-link.href = url;
-console.log(link);
-link.click();
-*/
-
-/* Try via xhr? Blocked by CORS.
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
-xhr.responseType = 'blob';
-xhr.onload = () => {
-    console.log('onload!')
-    var file = new Blob([xhr.response], {type: 'application/octet-stream'});
-    var link = document.createElement('a');
-    link.download = filename;//.substring(0, filename.lastIndexOf('.'));
-    link.href = window.URL.createObjectURL(file);
-    console.log(link);
-    link.click();
-}
-xhr.send();
-*/
-
-/* draw on canvas? Inscure operation
-let img = new Image();
-img.src = url;
-img.onload = () => {
-    let canvas = document.createElement("canvas");
-    let ctx = canvas.getContext("2d");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
-
-    var link = document.createElement('a');
-    const ext = filename.substring(filename.lastIndexOf('.'));
-    link.download = filename;
-    link.href = canvas.toDataURL(`image/png`);
-    console.log(link);
-    link.click();
-}
-*/
-// };
-
 
 export const base64EncodeUnicode = (content: string): string => {
     const utf8Bytes = encodeURIComponent(content).replace(/%([0-9A-F]{2})/g, function (match, g1) {
